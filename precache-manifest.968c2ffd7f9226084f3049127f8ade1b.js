9a8867d07de5864703bf7402d6390c4eb321c824
KarshBang <wswswzb@hotmail.com>
2020-05-24 16:42:33 +0800

    init

---
+++ precache-manifest.968c2ffd7f9226084f3049127f8ade1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c153fd4734c5bc794f3627fc45440084",
+    "url": "/sayeth-display/index.html"
+  },
+  {
+    "revision": "63ea9a5dfd2091f0c766",
+    "url": "/sayeth-display/static/css/main.d219995c.chunk.css"
+  },
+  {
+    "revision": "9984f774f64d993512cf",
+    "url": "/sayeth-display/static/js/2.f841bdc0.chunk.js"
+  },
+  {
+    "revision": "63ea9a5dfd2091f0c766",
+    "url": "/sayeth-display/static/js/main.fbd7bb5f.chunk.js"
+  },
+  {
+    "revision": "0965f7016ead1094c83a",
+    "url": "/sayeth-display/static/js/runtime-main.7b12400f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93c55f0a08dc033ef142bd0eb4973cbe.js"
+  "/sayeth-display/precache-manifest.968c2ffd7f9226084f3049127f8ade1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sayeth-display/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
