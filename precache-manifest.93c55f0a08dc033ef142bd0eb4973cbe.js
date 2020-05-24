6a6dc459f1e5af10dc7de0fe8e60b21378f83cdb
KarshBang <wswswzb@hotmail.com>
2020-05-24 15:57:28 +0800

    init

---
+++ precache-manifest.93c55f0a08dc033ef142bd0eb4973cbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f63a2ce0c7f97f7db47804f4f2df23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fdf2c31ab7fd638339f",
+    "url": "/static/css/main.d219995c.chunk.css"
+  },
+  {
+    "revision": "3779450d08e7ea62521e",
+    "url": "/static/js/2.69a9f3c6.chunk.js"
+  },
+  {
+    "revision": "7fdf2c31ab7fd638339f",
+    "url": "/static/js/main.2d76912b.chunk.js"
+  },
+  {
+    "revision": "6ee9628b164994f7e1d5",
+    "url": "/static/js/runtime-main.c459bd58.js"
+  }
+]);
